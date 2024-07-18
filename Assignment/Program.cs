41d8b03e0348287d41a2c80451a58625a0b8fbf8
ayanada739 <ayanada739@gmail.com>
2024-07-18 12:04:26 +0300

    Solving Part 01

---
+++ Assignment/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 01
+
+            #region Q01 What is the primary purpose of an interface in C#?
+            //- b) To define a blueprint for a class
+
+            #endregion
+
+            #region Q02 Which of the following is NOT a valid access modifier for interface members in C#?
+            //-a) private
+            //-b) protected
+
+
+            #endregion
+
+            #region Q03 Can an interface contain fields in C#?
+            //- b) No
+
+            #endregion
+
+            #region Q04 In C#, can an interface inherit from another interface?
+            //- b) Yes, interfaces can inherit from multiple interfaces
+
+            #endregion
+
+            #region Q05 Which keyword is used to implement an interface in a class in C#?
+            //- d) implements
+
+            #endregion
+
+            #region Q06 Can an interface contain static methods in C#?
+            //- a) Yes (starting from C# 8.0, interfaces can contain static methods)
+
+            #endregion
+
+            #region Q07 In C#, can an interface have explicit access modifiers for its members?
+            //- b) No, all members are implicitly public
+
+            #endregion
+
+            #region Q08 What is the purpose of an explicit interface implementation in C#?
+            //- b) To provide a clear separation between interface and class members
+
+            #endregion
+
+            #region Q09 In C#, can an interface have a constructor?
+            //- b) No, interfaces cannot have constructors
+
+            #endregion
+
+            #region Q10 How can a C# class implement multiple interfaces?
+            //- c) By separating interface names with commas
+
+            #endregion
+
+            #endregion
+
+
+        }
+    }
+}
