83e0a8c95258c9c605237a9d13c4c0f4bce69850
ayanada739 <ayanada739@gmail.com>
2024-07-18 13:20:04 +0300

    Interface

---
+++ OOP 04/Interfaces/IMyType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_04.Interfaces
+{
+    // Default Access Modifier Of Interface ==>Internat
+    //Developer 01
+    internal interface IMyType
+    {
+        // Default Access Modifier Inside Interface Public
+        // Private Access Modifier is not allowed inside interface
+        //What You Can Write Inside The Interface
+
+        // 1. Signature for Property
+        public int Id { get; set; }
+        // Inside Class Or Struct Automatic Property
+        // Compiler Witt generate Private Hidden Attribute (Backing Field]
+        // Inside Interface ==> Signature For Property
+
+
+        // 2. Signature for method [Name - Return Type - Parameter]
+        public void MyFun(int X);
+
+
+        // 3. Default Implemented Method [8.0 Feature (.Net core 3.1 2019)]
+        public void Print()
+        {
+            Console.WriteLine("Hello Default Implemented Method From Interface");
+        }
+
+
+
+    }
+}
--- OOP 04/Program.cs
+++ OOP 04/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace OOP_04
+﻿using OOP_04.Interfaces;
+
+namespace OOP_04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Interface
+
+            ////IMyType myType = new IMyType(); // Invalid
+            //// You Can not Create an object from interface
+            ////IMyType myType;
+            //// you can create a reference from interface
+            //// CLR Will Allocate Bytes For Reference "my Type"
+            //// References "my Type" Refer To Null
+            ////References "myType" Can referto an Object From Class Or struct Implement Interface
+
+            ////myType. Id= 20; // Invalid myType Refer To Null
+            ////myType.myFun(1); // Invalid myType Refer To Null
+            ////myType. Print(); // Invalid my Type Refer To Null
+
+            ////MyType my Type1= new MyType();
+            ////myType1.Id = 30;
+            ////myType1. MyFun( 100) ;
+            //////myType1. Print(); // Invalid [Default Implemented Method]
+            /////
+            //IMyType myType = new MyType();
+            //// Ref From Interface --> Object From Class Implement Interface
+            //myType.Id = 60;
+            //myType.MyFun(600);
+            //myType.Print();
+
+            #endregion
+
+
         }
-    }
+}
 }
